f7239bee632f363f2877045e69b09b44b855f1a5
Harshavardhan Sai Prakash <mara.sai.prakash@accenture.com>
2025-09-24 12:14:39 +0530

    Change in calculator code

--- calculator.py
+++ calculator.py
@@ -1,8 +1,13 @@
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return a/b
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
